990cc88d370bbde599fa35f4106ad6caaecb5700
b <cyru55@hi2.in>
2023-03-08 01:55:42 +0000

    Revert "8001/12/17 do not clear domains database while user update the app"
    This reverts commit cec8509791e3223c4c3d4ead14dad5b3565d1643.

--- app/src/main/java/co/bh/rekcuf/sniffer/SQLite.java
+++ app/src/main/java/co/bh/rekcuf/sniffer/SQLite.java
@@ -32,7 +32,7 @@ public class SQLite extends SQLiteOpenHelper{
 
 	@Override
 	public void onUpgrade(SQLiteDatabase db,int oldVersion,int newVersion){
-		//db.execSQL("drop table if exists host;");
+		db.execSQL("drop table if exists host;");
 		db.execSQL("drop table if exists data;");
 		onCreate(db);
 	}
